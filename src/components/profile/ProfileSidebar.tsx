ff40817e1b0f5c0018b8a46080c2dfa844c5b2e2
Apurv Gupta <apurv.gupta@dotsquares.com>
2025-12-09 16:43:27 +0530

    Icon compoennt

--- src/components/profile/ProfileSidebar.tsx
+++ src/components/profile/ProfileSidebar.tsx
@@ -48,8 +48,8 @@ export const ProfileSidebar = ({
       {/* Menu Items */}
       <nav className="space-y-2">
         {menuItems.map(item => {
-          const IconComponent = item.icon
-          const isReactElement = isValidElement(IconComponent)
+          const iconProp = item.icon
+          const isReactElement = isValidElement(iconProp)
 
           return (
             <button
@@ -63,11 +63,12 @@ export const ProfileSidebar = ({
             >
               <div className="flex items-center gap-3">
                 <span className="w-5 h-5 flex items-center justify-center">
-                  {isReactElement ? (
-                    IconComponent
-                  ) : (
-                    <IconComponent className="w-5 h-5" />
-                  )}
+                  {isReactElement
+                    ? iconProp
+                    : (() => {
+                        const IconComponent = iconProp as LucideIcon
+                        return <IconComponent className="w-5 h-5" />
+                      })()}
                 </span>
                 <span className="font-medium">{item.label}</span>
               </div>
